c03c81a0d2121f0d459715771daf5efd256cdaa4
Shawn Kemp <shawn.kemp@gmail.com>
2024-05-26 10:10:21 -0700

    fixed save colors to txt

--- soko/myproject.js
+++ soko/myproject.js
@@ -475,7 +475,7 @@ document.addEventListener('keypress', (event) => {
             }; 
             console.log(key.reverse())
             var content = JSON.stringify(key.reverse())
-            var filename = tokenData.hash + ".txt";
+            var filename = $fx.hash + ".txt";
             var blob = new Blob([content], {type: "text/plain;charset=utf-8"});
             saveAs(blob, filename);
             }
